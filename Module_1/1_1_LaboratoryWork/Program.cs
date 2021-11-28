de8d5f99d1d5aacefc2939f79dd2ab703648f454
maxim <milovanov160386@gmail.com>
2021-11-28 23:02:53 +0300

    Module 1 done

---
+++ Module_1/1_1_LaboratoryWork/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace Module_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circl circle1 = new Circl(23);
+            Console.WriteLine(circle1);
+            Circl circl2 = circle1.NewCircleByArea(60);
+            Console.WriteLine(circl2);
+        }
+    }
+    
+    class Circl
+    {
+        
+        public double Radius { get; private set; }
+        public double Area { get; private set; }
+
+        public Circl(double radius)
+        {
+            Radius = radius;
+            Area = Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public Circl NewCirclByRadius(double radius)
+        {
+            return new Circl(radius);
+        }
+
+        public Circl NewCircleByArea(double area)
+        {
+            double radius = Math.Sqrt(area / Math.PI);
+            return new Circl(radius);
+        }
+
+        public override string ToString()
+        {
+            return $"Radius = {Radius} мм;  Area = {Area} мм^2";
+        }
+    } 
+}
---
+++ Module_1/2_Task/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2_Task
+{
+    interface IShape2D
+    {
+        int CoorX { get; set; }
+        int CoorY { get; set; }
+        public void Draw();
+    }
+    
+    public struct Point2D
+    {
+        public int X, Y;
+        public string PointName;
+        public Point2D(int x, int y, string point_name)
+        {
+            X = x;
+            Y = y;
+            PointName = point_name;
+        }
+
+        public override string ToString()
+        {
+            return $"Point({PointName}) (X={X}; Y={Y})";
+        }
+    }
+
+    struct Triangle : IShape2D
+    {
+        public List<Point2D> Points;
+        
+        public int CoorX { get; set; }
+        public int CoorY { get; set; }
+        public void Draw()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Draw a triangle at position with coordinates: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write($"(X = {CoorX}; Y = {CoorY})\n");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nCoordinates triangle points:");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{Points[0]}\n{Points[1]}\n{Points[2]}", ConsoleColor.Green);
+            Console.ResetColor();
+        }
+
+        public Triangle(int X, int Y, List<Point2D> points)
+        {
+            CoorX = X;
+            CoorY = Y; 
+            Points = points; 
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Point2D> points = new List<Point2D> { 
+                new Point2D {X = 6, Y = 5, PointName="A"},
+                new Point2D {X = 8, Y = 2, PointName="B"},
+                new Point2D {X = 5, Y = 25, PointName="C"}};
+            Triangle triangle = new Triangle(10, 52, points);
+            triangle.Draw();
+        }
+    }
+}
